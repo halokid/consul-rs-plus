9598c34603c3c59bbf37b6f430e39c07a3640296
r0x <r00txxyyzz@gmail.com>
2021-07-02 19:41:52 +0800

    check set_with_session status and try again

--- src/kv.rs
+++ src/kv.rs
@@ -1,5 +1,7 @@
-use crate::Client;
+use crate::{Client, PKGX};
 use std::io::Read;
+use std::{thread, time};
+use std::borrow::Borrow;
 
 #[derive(Serialize, Deserialize)]
 #[derive(Debug, Eq, PartialEq)]
@@ -20,16 +22,15 @@ fn default_string() -> String {
 }
 
 impl KVPair {
-
   pub fn new() -> Self {
-    KVPair{
+    KVPair {
       key: "".to_string(),
       create_index: 0,
       modify_index: 0,
       lock_index: 0,
       flags: 0,
       value: "".to_string(),
-      session: "".to_string()
+      session: "".to_string(),
     }
   }
 
@@ -49,24 +50,62 @@ impl KVPair {
       .put(&url)
       .body(v.into())
       .send()
-      .map_err( |e| e.to_string())?;
+      .map_err(|e| e.to_string())?;
     let mut body = String::new();
-    rsp.read_to_string(&mut body).map_err( |e| e.to_string() )?;
+    rsp.read_to_string(&mut body).map_err(|e| e.to_string())?;
     return Ok(body.as_str().contains("true"));
   }
 
   pub fn set_with_session<S: Into<String>>(&self, c: &Client, key: S, v: S, session: S)
-                      -> Result<bool, String> {
+                                           -> Result<bool, String> {
     let url = format!("http://{}:{}/v1/kv/{}?acquire={}", c.host, c.port,
-                              key.into(), session.into());
+                      key.into(), session.into());
+    let mut vx = v.into();
+    let vxx = vx.clone();   //  todo: 这里定义 vxx 是不会错误的
+    let xx = &vxx.to_string();
+    let yy = vxx.as_str().to_string();
+
+    let zz = &vxx;
+    let kk = vxx.as_str();
+
+    // todo: 要注意上面几种类型的变化， 简单来说消除 value borrowed here after move 这种错误， 一般都是用引用
+
     let mut rsp = reqwest::Client::new()
       .put(&url)
-      .body(v.into())
+      // .body(v.into())
+      .body(vx)
       .send()
-      .map_err( |e| e.to_string())?;
+      .map_err(|e| e.to_string())?;
     let mut body = String::new();
-    rsp.read_to_string(&mut body).map_err( |e| e.to_string() )?;
-    return Ok(body.as_str().contains("true"));
+    rsp.read_to_string(&mut body).map_err(|e| e.to_string())?;
+    PKGX.debug_print(format!("set_with_session debug: {:?}", body).as_str());
+    // return Ok(body.as_str().contains("true"));
+    if !body.as_str().contains("true") {
+      // let vx = v.borrow();
+      let mut loop_flag = true;
+      while loop_flag {
+        // let x = &vx.to_string();
+        // let vxx = xv.into();
+        // let vxx = vx.clone();   // todo: 错误 value borrowed here after move 的问题是出现在这里， 因为 while循环的时候， 要每一个执行了 rsp 的封装都会转移 vxx 的所有权
+        let mut rsp = reqwest::Client::new()
+          .put(&url)
+          // .body("xxxx")
+          // .body(&vxx.to_string()) // todo: 这样会出错 the trait `From<&std::string::String>` is not implemented for `reqwest::Body`
+          .body(vxx.as_str().to_string())
+          // .body(vxx.clone())
+          .send()
+          .map_err(|e| e.to_string())?;
+        let mut body = String::new();
+        rsp.read_to_string(&mut body).map_err(|e| e.to_string())?;
+        PKGX.debug_print(format!("set_with_session loop debug: {:?}", body).as_str());
+        thread::sleep(time::Duration::from_secs(2));
+
+        if body.as_str().contains("true") {
+          loop_flag = false
+        }
+      }
+    }
+    Ok(true)
   }
 
   pub fn delete<S: Into<String>>(&self, c: &Client, key: S) -> Result<bool, String> {
@@ -74,9 +113,9 @@ impl KVPair {
     let mut rsp = reqwest::Client::new()
       .delete(&url)
       .send()
-      .map_err( |e| e.to_string())?;
+      .map_err(|e| e.to_string())?;
     let mut body = String::new();
-    rsp.read_to_string(&mut body).map_err( | e| e.to_string() )?;
+    rsp.read_to_string(&mut body).map_err(|e| e.to_string())?;
     return Ok(body.as_str().contains("true"));
   }
 
