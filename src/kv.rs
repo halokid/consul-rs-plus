d9cbb4083bfcb3aacc7481fc161eebdd20a8116e
aoh <r00txxyyzz@gmail.com>
2021-08-27 10:09:59 +0800

    Update kv.rs

--- src/kv.rs
+++ src/kv.rs
@@ -1,195 +1,179 @@
-use crate::{Client};
+#![allow(dead_code)]
+
+#[macro_use]
+extern crate serde_derive;
+extern crate base64;
+extern crate reqwest;
+extern crate serde_json;
+
+pub mod kv;
+mod config;
+pub mod session;
+pub mod pkg;
+
+use self::kv::*;
 use std::io::Read;
-use std::{thread, time};
-use std::borrow::Borrow;
 use crate::pkg::CustomError;
+use base64::{decode};
 
-#[derive(Serialize, Deserialize)]
-#[derive(Debug, Eq, PartialEq)]
-#[serde(rename_all = "PascalCase")]
-pub struct KVPair {
-  pub Key: String,
-  pub CreateIndex: u64,
-  pub ModifyIndex: u64,
-  pub LockIndex: u64,
-  pub Flags: u64,
-  pub Value: String,
-  #[serde(default = "default_string")]
-  pub Session: String,
+// todo: global use varible here
+// pub const PKGX: pkg::Pkg = pkg::Pkg::new(true);
+
+pub struct Client {
+  pub debug: bool,
+
+  host: String,
+  port: u16,
+
+  kv:   KVPair,
+  session: session::Session,
 }
 
-fn default_string() -> String {
-  "".to_string()
-}
-
-impl KVPair {
-  pub fn new() -> Self {
-    KVPair {
-      Key: "".to_string(),
-      CreateIndex: 0,
-      ModifyIndex: 0,
-      LockIndex: 0,
-      Flags: 0,
-      Value: "".to_string(),
-      Session: "".to_string(),
+impl Client {
+  pub fn new<S: Into<String>>(host: S, port: u16) -> Client {
+    Client {
+      debug: false,
+      host: host.into(),
+      port: port,
+      kv: KVPair::new(),
+      session: session::Session::new(),
     }
   }
 
-  // todo: Into<String> get the ownship for varible?
-  pub fn get<S: Into<String>>(&self, c: &Client, key: S) -> Result<Vec<KVPair>, String> {
-    let url = format!("http://{}:{}/v1/kv/{}", c.host, c.port, key.into());
-    let mut rsp = reqwest::get(&url).map_err(|e| e.to_string())?;
-    let mut body = String::new();
-    rsp.read_to_string(&mut body).map_err(|e| e.to_string())?;
-    // todo: success -> return Vec<KVPair>,  fail -> return error string
-    c.debug_print(format!("kv get body --- {}", body).as_str());
-    serde_json::from_str::<Vec<KVPair>>(&body).map_err(|e| e.to_string())
+  pub fn debug_print(&self, s: &str) {
+    if self.debug {
+      println!("[DEBUG] --- {:?}", s);
+    }
   }
 
-  pub fn get_folder_keys<S: Into<String>>(&self, c: &Client, key: S) -> Result<String,
-                String> {
-    let url = format!("http://{}:{}/v1/kv/{}/?keys", c.host, c.port, key.into());
-    let mut rsp = reqwest::get(&url).map_err(|e| e.to_string())?;
-    let mut body = String::new();
-    rsp.read_to_string(&mut body).map_err(|e| e.to_string())?;
-    Ok(body)
-  }
-
-  pub fn set<S: Into<String>>(&self, c: &Client, key: S, v: S) -> Result<bool, String> {
-    let url = format!("http://{}:{}/v1/kv/{}", c.host, c.port, key.into());
-    let mut rsp = reqwest::Client::new()
-      .put(&url)
-      .body(v.into())
-      .send()
-      .map_err(|e| e.to_string())?;
-    let mut body = String::new();
-    rsp.read_to_string(&mut body).map_err(|e| e.to_string())?;
-    return Ok(body.as_str().contains("true"));
-  }
-
-  pub fn set_with_session<S: Into<String>>(&self, c: &Client, key: S, v: S, session: S)
-                                           -> Result<bool, String> {
-    let url = format!("http://{}:{}/v1/kv/{}?acquire={}", c.host, c.port,
-                      key.into(), session.into());
-    let mut vx = v.into();
-    c.debug_print(format!("key set_with_session val: {:?}", vx).as_str(), );
-    // let vxx = vx.clone();
-
-    let mut rsp = reqwest::Client::new()
-      .put(&url)
-      // .body(v.into())
-      .body(vx)
-      .send()
-      .map_err(|e| e.to_string())?;
-    let mut body = String::new();
-    rsp.read_to_string(&mut body).map_err(|e| e.to_string())?;
-    c.debug_print(format!("set_with_session debug: {:?}", body).as_str(), );
-    return Ok(body.as_str().contains("true"));
-    /*
-    if !body.as_str().contains("true") {
-      let mut loop_flag = true;
-      let mut loop_num = 0;
-      while loop_flag && loop_num < 10 {
-        c.debug_print("---loop_flag set_with_session---");
-        let mut rsp = reqwest::Client::new()
-          .put(&url)
-          // todo: loop will get varible own, so use ref
-          .body(vxx.as_str().to_string())
-          .send()
-          .map_err(|e| e.to_string())?;
-        let mut body = String::new();
-        rsp.read_to_string(&mut body).map_err(|e| e.to_string())?;
-        c.debug_print(format!("set_with_session loop debug: {:?}", body).as_str());
-        // thread::sleep(time::Duration::from_secs(1));
-        if body.as_str().contains("true") {
-          loop_flag = false;
-          return Ok(true);
-        }
-        loop_num += 1;
-        thread::sleep(time::Duration::from_secs(19))
+  pub fn kv_get<S: Into<String>>(&self, key: S) -> String {
+    let res = self.kv.get(self, key);
+    match res {
+      Ok(kvs) => {
+        let kv = kvs.get(0).unwrap();
+        let val = &kv.Value;
+        let val_de = decode(val).unwrap();
+        let val_de_str = String::from_utf8(val_de).unwrap();
+        val_de_str
       }
-      return Ok(false);
-    }
-    Ok(true)
-     */
-  }
-
-  pub fn delete<S: Into<String>>(&self, c: &Client, key: S) -> Result<bool, String> {
-    let url = format!("http://{}:{}/v1/kv/{}", c.host, c.port, key.into());
-    let mut rsp = reqwest::Client::new()
-      .delete(&url)
-      .send()
-      .map_err(|e| e.to_string())?;
-    let mut body = String::new();
-    rsp.read_to_string(&mut body).map_err(|e| e.to_string())?;
-    return Ok(body.as_str().contains("true"));
-  }
-
-  pub fn delete_both_session<S: Into<String>>(&self, c: &Client, key: S) -> Result<bool, String> {
-    // get kv session
-    let keyx = key.into();
-    let keyx_ref = keyx.as_str();
-    let kv = self.get(c, keyx_ref.to_string());
-    match kv {
       Err(err) => {
-        return Err(format!("can not get kv {} {}", keyx_ref.to_string(), err));
+        err
       }
-      _ => {}
     }
-    let kvx = kv.unwrap();
-    c.debug_print(format!("kvx ------ {:?}", kvx).as_str());
-    let kvx_unwp = kvx.get(0).unwrap();
-    let sid = &kvx_unwp.Session;
-    c.debug_print(format!("delete_both_session sid: {}", sid).as_str());
-    //del session
-    c.session_delete(sid);
-
-    let url = format!("http://{}:{}/v1/kv/{}", c.host, c.port, keyx_ref.to_string());
-    let mut rsp = reqwest::Client::new()
-      .delete(&url)
-      .send()
-      .map_err(|e| e.to_string())?;
-    let mut body = String::new();
-    rsp.read_to_string(&mut body).map_err(|e| e.to_string())?;
-    return Ok(body.as_str().contains("true"));
   }
 
-  pub fn get_value(&self) -> Result<Vec<u8>, base64::DecodeError> {
-    base64::decode(&self.Value)
+  pub fn kv_get_folder<S: Into<String>>(&self, folder: S) -> Result<Vec<String>,
+    String> {
+    let folder_keys = self.kv.get_folder_keys(self, folder).unwrap();
+    serde_json::from_str::<Vec<String>>(&folder_keys).map_err(|e| e.to_string())
+  }
+
+  pub fn kv_set<S: Into<String>>(&self, key: S, v: S) -> Result<bool, String> {
+    self.kv.set(self, key, v)
+  }
+
+  pub fn kv_set_with_session<S: Into<String>>(&self, key: S, v: S, session: S)
+                          -> Result<bool, String> {
+    self.kv.set_with_session(self, key, v, session)
+  }
+
+  pub fn kv_delete<S: Into<String>>(&self, key: S) -> Result<bool, String> {
+    self.kv.delete(self, key)
+  }
+
+  pub fn kv_delete_both_session<S: Into<String>>(&self, key: S) -> Result<bool, String> {
+    self.kv.delete_both_session(self, key)
+  }
+
+  pub fn session_set(&self, lock_delay: String, name: String, node: String,
+                     behavior: String, ttl: String) -> String {
+    let mut s = session::Session::new();
+    s.LockDelay = "0.001s".to_string();
+    if name != "".to_string() {
+      s.name = name;
+    }
+
+    if node != "".to_string() {
+      let nodex = Some(node);
+      s.node = nodex;
+    }
+    s.Behavior = behavior;
+    s.TTL = ttl;
+    self.debug_print(format!("lib session set: {:?}", s).as_str(), );
+    self.session.set(self, &s)
+  }
+
+  pub fn session_renew(&self, sid: &str) -> Result<(), CustomError> {
+    self.session.renew(self, sid)
+  }
+
+  pub fn session_delete(&self, sid: &str) -> String {
+    self.session.delete(self, sid)
   }
 }
-
 
 #[cfg(test)]
 mod tests {
   use crate::Client;
+  use base64::Config;
+  use crate::config;
+  use crate::pkg::CustomError;
+  use crate::kv::KVPair;
 
   #[test]
-  fn unmarshal_kv_pair() {
-    let dat = r#"[
-        {
-            "LockIndex":666,
-            "Key":"key/path",
-            "Flags":0,
-            "Value":"dHJ1ZQ==",
-            "CreateIndex":495627,
-            "ModifyIndex":495627
-        }]"#;
-    let v: Vec<super::KVPair> = serde_json::from_str(dat).unwrap();
-    assert_eq!(v[0].LockIndex, 666);
-    assert_eq!(v[0].get_value().unwrap(), "true".as_bytes().to_owned())
+  fn test_kv_delete_both_session() {
+    let host = config::CONFIG["consul_addr"];
+    let client = Client::new(host, 8500);
+    let res = client.kv_delete_both_session("my-key");
+    println!("res ---------- {}", res.unwrap());
   }
 
-  // #[test]
-  // fn test_get_kv() {
-  //   let client = Client::new("8.8.8.8", 8500);
-  //   let my_keys = client.kv_get("my-key").unwrap();
-  //   for k in my_keys {
-  //     println!("k: {:?}", k);
-  //   }
-  // }
-}
+  #[test]
+  fn test_kv_get() {
+    let host = config::CONFIG["consul_addr"];
+    let client = Client::new(host, 8500);
+    let val = client.kv_get("my-key");
+    println!("val ----- {}", val);
+  }
 
+  #[test]
+  fn test_session_set() {
+    let host = config::CONFIG["consul_addr"];
+    let client = Client::new(host, 8500);
+    let se = client.session_set("15s".to_string(), "my-session".to_string(), "node1".to_string(), "release".to_string(), "10m0s".to_string());
+  }
+
+  #[test]
+  fn test_kv_set_with_session() {
+    let host = config::CONFIG["consul_addr"];
+    let client = Client::new(host, 8500);
+    let session = client.session_set("15s".to_string(), "my-session".to_string(), "node1".to_string(), "release".to_string(), "10m0s".to_string());
+
+    let res = client.kv_set_with_session("my-key", "my-val", session.as_str()).unwrap();
+    println!("res ------- {}", res);
+  }
+
+  #[test]
+  fn test_session_renew() {
+    let host = config::CONFIG["consul_addr"];
+    let client = Client::new(host, 8500);
+    let ok = client.session_renew("d5663534-82f9-429b-954c-ae63d59d3502");
+    match ok {
+      Ok(_) => { println!("---ok---");}
+      Err(_) => { println!("---err---");}
+    }
+  }
+
+  #[test]
+  fn test_kv_ge_folder() {
+    let host = config::CONFIG["consul_addr"];
+    let client = Client::new(host, 8500);
+    let res = client.kv_get_folder("pomid/Echo");
+    let svcs = res.unwrap();
+    println!("svcs ------------- {:?}", svcs);
+  }
+
+}
 
 
 
