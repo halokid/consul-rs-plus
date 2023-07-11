1b546dff00d7e1de5be9feb0e20b11779909cafd
r0x <r0x@r0x.org>
2023-07-11 22:35:02 +0800

    coding service

--- src/service.rs
+++ src/service.rs
@@ -7,8 +7,10 @@ pub struct Service {
 
 impl Service {
 
-  /// Get the health services, url `/v1/health/checks/:service_name`
-  pub fn get(&self, c: &Client, service_name: &str) -> Result<Vec<String>, Error> {
+  /// Get the health services, base on below API url
+  /// http://localhost:8500/v1/catalog/service/neon_broker
+  /// http://localhost:8500/v1/health/checks/neon_broker
+  pub fn get(&self, c: &Client, service_name: &str) -> Result<Vec<String>, dyn Error> {
     let url = format!("http://{}:{}/v1/health/checks/{}", c.host, c.port, service_name);
 
     Ok(vec![])
--- src/vo.rs
+++ src/vo.rs
@@ -1,1 +1,21 @@
 /// Vo
+pub struct Service {
+  node: String,
+  check_id: String,
+  name: String,
+  status: String,
+  notes: String,
+  output: String,
+  service_id: String,
+  service_name: String,
+  service_tags: String,
+  r#type: String,
+  interval: String,
+  timeout: String,
+  exposed_port: u64,
+  definition: String,
+  create_index: u64,
+  modify_index: u64,
+}
+
+
