28b41982881d8e3b79a19287cc4802069b097bb4
r0x <r0x@r0x.org>
2023-07-13 11:19:55 +0800

    coding service

--- examples/service.rs
+++ examples/service.rs
@@ -3,11 +3,35 @@ use consul_rs_plus::service::Service;
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-  let client = Client::new("consul_test", 8500);
+  println!("-->>> service examples");
+
+  let client = Client::new("localhost", 8500);
   let s = Service::new();
   s._get_nodes(&client, "neon_broker").await;
 
-  println!("-->>> service examples");
+  // for dynamic json
+  let url = "http://localhost:8500/v1/catalog/service/neon_broker";
+
+  let rsp = reqwest::Client::new().get(url).send().await.unwrap();
+  let rsp_json: serde_json::Value = rsp.json();
+  println!("{:#?}", rsp_json);
+
+  /*
+  let echo_json: serde_json::Value = reqwest::Client::new()
+    .post("https://jsonplaceholder.typicode.com/posts")
+    .json(&serde_json::json!({
+            "title": "Reqwest.rs",
+            "body": "https://docs.rs/reqwest",
+            "userId": 1
+        }))
+    .send()
+    .await?
+    .json()
+    .await?;
+
+  println!("{:#?}", echo_json);
+   */
+
   Ok(())
 }
 
